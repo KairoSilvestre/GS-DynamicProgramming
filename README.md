25a558832eadd295d97accab209da385c056f37d
KairoSilvestre <kairodasilva.carvalho@gmail.com>
2025-11-14 18:09:48 -0300

    Projeto GS - Corrigindo

---
+++ README.md
@@ -0,0 +1,93 @@
+# Requalificação Profissional - Global Solution
+
+### Integrantes:
+Guilherme Santos Nunes - RM558989
+Kaique Rodrigues Zaffarani - RM 556677 
+Kairo da Silva Silvestre de Carvalho - RM558288 
+
+## Contextualização do problema
+O cenário profissional atual, passa por uma tranformação sem precendentes, impulsionadas pela automação, inteligência artificial e novos modelos de negócios. Estimativas apontam que 65% dos trabalhadores precisarão se requalificar para manter a empregabilidade. 
+
+Neste contexto, a tomada de decisão sobre quais competências desenvolver torna-se um problema de
+otimização. Um profissional dispõe de recursos limitados (tempo e, por vezes, dinheiro) e precisa alocá-los da
+forma mais eficiente para maximizar sua relevância no mercado
+
+## Desafio - Dynamic Programming 
+Esse projeto aplica Programação Dinâmica para resolver um problema real de requalificação profissional usando a versão 0/1 do problema de Mochila. 
+
+O sistema implementa duas abordagens, como: 
+
+* Memoização (Top-Down)
+* Tabulação (Bottom-up)
+
+Ao chegar no final do programa, ele compara os resultados para garantir que ambas cheguem ao mesmo resultado ótimo, além da lista de cursos. 
+
+## Objetivo do Projeto
+Simular, por meio de Programação Dinâmica, a escolha de cursos ideias para um profissional que precisa se requalificar para carreiras do futuro, mas possui tempo limitado. 
+
+Cada curso possui:
+
+* Hours (peso da mochila): quantas horas ele exige. 
+* Impact (valor da mochila): o quanto ele agrega à carreira (1-100).
+
+O problema é determinar: 
+```
+"Qual conjunto de cursos maximiza o impacto total sem ultrapassar o tempo disponível?"
+```
+
+Este é exatamente o Problema da Mochila 0/1, onde cada curso pode seer escolhido uma única vez. 
+ 
+## Leitura do catálogo de cursos 
+
+O programa lê o arquivo courses_reskilling.json, que funciona como um banco de dados contendo todos os cursos, suas horas, impacto e categorias. Esses JSON pode ser modificado sem alterar o código. 
+## Entrada do usuário 
+
+O usuário informa quantas horas tem disponíveis. Esse valor é a capacidade da mochila. 
+## Execução de dois algoritmos de Programação Dinâmica.
+
+### Memoização (Top-Down)
+
+* Usa recursão e chace (@Iru_cache) para evitar cálculos repetidos.
+* Resolve a recorrência da mochila partindo do problema maior e quebrando em subproblemas menores. 
+
+### Tabulação (Bottom-Up)
+
+* Constrói uma tabela 2D onde cada posição representa o melhor impacto possível para um número específico de cursos e horas disponíveis. 
+* Resolve de forma totalmente iterativa.
+
+Ambos retornam: 
+
+* O impacto total máximo.
+* A lista de cursos escolhidos.
+
+## Reconstrução dos cursos selecionados 
+
+Após encontrar o melhor valor, o algoritmo faz o caminho inverso na tabela/recursão para descobrir quais cursos compõem a solução ótima. 
+
+## Verificação final 
+O programa confirma: 
+
+* Se ambos os algoritmos tiveram o mesmo impacto total. 
+* Se escolherem a mesma combinação de cursoos. 
+
+Se sim:
+```bash 
+verificação: Ambos os métodos retornam o mesmo resultado!
+```
+## Como executar o Projeto 
+
+### 1. Rodar programa
+No terminal: 
+```bash
+python main.py
+```    
+### 2. Interação
+O programa exibirá os cursos do catálogo e pedirá seu tempo disponível: 
+```bash
+Informe seu tempo disponível em horas: 180
+```
+Em seguida, executará os dois algoritmos e retornará: 
+
+* O impacto total máximo. 
+* A lista de cursos recomendados 
+* A verificação final da consistência
---
+++ main.py
@@ -0,0 +1,164 @@
+import json
+from functools import lru_cache
+from typing import List, Dict, Tuple
+
+#  Leitura do catálogo - Parte 1 
+
+def carregar_cursos(caminho_json: str) -> List[Dict]:
+    with open(caminho_json, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+# Parte 2 - Exibir todos os cursos lidos do JSON, listando eles para o usuário.
+
+def exibir_catalogo(cursos: List[Dict]):
+    print("\n=== Catálogo dos Cursos Disponíveis ===")
+    for c in cursos:
+        print(
+            f"{c['id']:>2}. {c['name']} ({c['hours']}h, impacto {c['impact']}) "
+            f"- {c['category']}"
+        )
+    print("======================================")
+
+
+#  Algoritmo Knapsack (Top-down Memo)
+
+def knapsack_memo(cursos: List[Dict], capacidade: int) -> Tuple[int, List[Dict]]:
+    n = len(cursos) 
+    # Define o número de cursos (n)
+    # A função receberá a lista de cursos e o tempo total disponível (a capacidade da mochila)
+
+    @lru_cache(maxsize=None)
+    def dp(i: int, w: int) -> int:
+
+        # Iru_cache guarda os resultados de cada combinação
+
+        if i == 0 or w == 0:
+            return 0
+        
+        # Se não há cursos / ou não há tempo = o impacto é zero 
+
+        curso = cursos[i - 1]
+        wi = curso["hours"]
+        vi = curso["impact"]
+
+        # Será separado o curso atual e separado em seus dois atributos principais: peso e impacto 
+
+        if wi > w:
+            return dp(i - 1, w)
+        
+        # Se o tempo restante não cauber, não podemos escolher. 
+
+        return max(dp(i - 1, w), vi + dp(i - 1, w - wi))
+
+        # O problema da mochila é resolvido aqui, onde é comparado as duas situações. Isso permite a garantia do resultado "ótimo".
+    
+    valor_otimo = dp(n, capacidade)
+
+    # reconstrução
+    selecionados = []
+    i, w = n, capacidade
+    while i > 0 and w > 0:
+        if dp(i, w) != dp(i - 1, w):
+            curso = cursos[i - 1]
+            selecionados.append(curso)
+            w -= curso["hours"]
+        i -= 1
+
+        # Essa parte do algoritmo reconstrói a lista de cursos escolhidos, andando de trás pra frente. Se o valor mudou no dp(i, w) e no dp(i-1, w), significa que esse curso foi escolhido. 
+
+    selecionados.reverse()
+    return valor_otimo, selecionados
+
+    # No fim, a lista é invertida para manter na ordem original. 
+
+#  Algoritmo Knapsack (Bottom-up Tab)
+
+def knapsack_tab(cursos: List[Dict], capacidade: int) -> Tuple[int, List[Dict]]:
+    n = len(cursos)
+    dp = [[0] * (capacidade + 1) for _ in range(n + 1)]
+
+    # Aqui criamos uma tabela (matriz) com n + 1 linhas (cursos) e capacidade + 1 colunas (horas)
+
+    for i in range(1, n + 1):
+        wi = cursos[i - 1]["hours"]
+        vi = cursos[i - 1]["impact"]
+        for w in range(capacidade + 1): 
+
+            # Percorre todos os cursos (i) e todas as capacidades (w), preenchendo a tabala passa a passo.
+
+            if wi > w:
+                dp[i][w] = dp[i - 1][w]
+            else:
+                dp[i][w] = max(dp[i - 1][w], vi + dp[i - 1][w - wi])
+
+                # A lógica é a seguite: se o curso não cabe, ele copia o valor de cima
+
+                # Se cabe, ele compara "pegar" ou "não pegar" e guardar o maior.
+
+    valor_otimo = dp[n][capacidade]
+
+    # Aqui é o impacto máximo possível. 
+
+    # reconstrução
+    selecionados = []
+    i, w = n, capacidade
+    while i > 0 and w > 0:
+        if dp[i][w] != dp[i - 1][w]:
+            curso = cursos[i - 1]
+            selecionados.append(curso)
+            w -= curso["hours"]
+        i -= 1
+
+    selecionados.reverse()
+    return valor_otimo, selecionados
+
+# Na reconstrução, ocorre o mesmo processo do top-down, percorrendo de baixo para cima verificando quais cursos foram escolidos. 
+
+#  Execução principal
+
+def main():
+    caminho_json = "courses_reskilling.json"  
+    cursos = carregar_cursos(caminho_json)
+    exibir_catalogo(cursos)
+
+    try:
+        capacidade = int(input("\nColoque seu tempo disponível em horas: "))
+        if capacidade <= 0:
+            raise ValueError
+    except ValueError:
+        print("Entrada inválida. Use um número inteiro positivo.")
+        return
+    
+    # Até aqui, o algoritmo fará duas ações como: ler o arquivo JSON e converter o valor digitado do usuário para inteiro 
+
+    print("\nExecutando algoritmo com Memoização...")
+    valor_memo, escolhidos_memo = knapsack_memo(cursos, capacidade)
+
+    print("Executando algoritmo com Tabulação...")
+    valor_tab, escolhidos_tab = knapsack_tab(cursos, capacidade)
+
+    # Aqui é onde os dois algoritmos são executados. A memoização resolve o problema de forma recursiva e a tabulação de forma iterativa. 
+
+    print("\n=== Resultado Final ===")
+    print(f"Tempo disponível: {capacidade}h")
+    print(f"Valor máximo (Impacto total): {valor_tab}")
+
+    print("\nCursos recomendados:")
+    for c in escolhidos_tab:
+        print(f"- {c['name']} ({c['hours']}h, impacto {c['impact']})")
+
+        # Os cursos escolhidos representam a melhor combinação possível, ou seja, o conjunto que oferece o maior impacto dentro do tempo limitado do usuário. 
+
+    # Comparação final
+    conjuntos_iguais = {c["id"] for c in escolhidos_memo} == {c["id"] for c in escolhidos_tab}
+    if valor_memo == valor_tab and conjuntos_iguais:
+        print("\nVerificação: Ambos os métodos retornaram o mesmo resultado!")
+    else:
+        print("\nVerificação: Resultados diferentes!")
+
+        # Essa parte comprova o uso de ambos os métodos. Se o resultado foi o mesmo, sabemos que a nossa implementação está correta.
+
+
+if __name__ == "__main__":
+    main()
+
